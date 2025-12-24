17d02016acb3bafba03e0fa3ecbc420792116d6f
Mayur Nishad <mayurnish18@gmail.com>
2025-12-24 11:40:38 +0530

    committ

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Promo from "@/components/Promo";
 import Cart from "@/components/Cart";
 export default function HomePage() {
   return (
-    <div className="space-y-20 md:py-20 md:px-20">
+    <div className="space-y-20 md:py-20 md:px-20 py-12">
       {/* HERO SECTION */}
       <Hero />
       {/* CATEGORIES */}
